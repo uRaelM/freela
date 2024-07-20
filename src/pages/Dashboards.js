65d2d455f5dcf89f14a47c0fb9327e2d777fce0d
uRaelM <rafael.dsm3@gmail.com>
2024-07-20 19:15:43 -0300

    transição do front-end completa

--- src/pages/Dashboards.js
+++ src/pages/Dashboards.js
@@ -1,6 +1,177 @@
+import React, { useEffect, useState } from "react";
 import "./css/global.css";
 import "./css/dashboards.css";
 
+import Programacao from "./icons/Programacao";
+import Design from "./icons/Design";
+import Marketing from "./icons/Marketing";
+import Edicao from "./icons/Edicao";
+import Consultoria from "./icons/Consultoria";
+
 export default function Dashboards() {
-  return <h1>teste</h1>;
+  const [films, setFilms] = useState([]);
+  const [resultsCount, setResultsCount] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsVisible(!isVisible);
+  };
+
+  useEffect(() => {
+    // Substitua a URL pela sua API
+    fetch("/api/films")
+      .then((response) => response.json())
+      .then((data) => {
+        setFilms(data);
+        setResultsCount(data.length);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <>
+      <header>
+        <div className="header-content">
+          <span className="logo">
+            Freela
+            <span style={{ color: "var(--default-green)", fontSize: "3rem" }}>
+              .
+            </span>
+          </span>
+          <span
+            className="profile"
+            onClick={toggleDropdown}
+            style={{ marginLeft: "auto", userSelect: "none" }}
+          >
+            Profile
+          </span>
+          <div className="wrapper">
+            <div
+              id="DropDown"
+              className={`dropdown ${isVisible ? "" : "hidden"}`}
+            >
+              <ul>
+                <li>
+                  <a href="crud/add.php">Tornece um prestador</a>
+                </li>
+                <li style={{ color: "#ef476f", borderBottom: "none" }}>
+                  Logout
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+      <section id="banner">
+        <div className="banner">
+          <h1>
+            Encontre o
+            <span style={{ color: "var(--default-green)" }}>
+              serviço de freelance
+            </span>
+            que você precisa agora mesmo
+          </h1>
+          <input
+            type="text"
+            name="search"
+            placeholder="Pesquise por qualquer serviço"
+          />
+        </div>
+      </section>
+
+      <h1 className="cardstext">Os mais procurados</h1>
+
+      <section id="cards">
+        <div className="card" style={{ backgroundColor: "var(--default-red)" }}>
+          <div className="white-svg">
+            <Programacao />
+          </div>
+          <h2>Programação</h2>
+        </div>
+
+        <div
+          className="card"
+          style={{ backgroundColor: "var(--default-yellow)" }}
+        >
+          <div className="white-svg">
+            <Design />
+          </div>
+          <h2>Design</h2>
+        </div>
+
+        <div
+          className="card"
+          style={{ backgroundColor: "var(--default-green)" }}
+        >
+          <div className="white-svg">
+            <Marketing />
+          </div>
+          <h2>Marketing</h2>
+        </div>
+
+        <div
+          className="card"
+          style={{ backgroundColor: "var(--default-blue)" }}
+        >
+          <div className="white-svg">
+            <Edicao />
+          </div>
+          <h2>Marketing</h2>
+        </div>
+
+        <div
+          className="card"
+          style={{ backgroundColor: "var(--default-dark-green)" }}
+        >
+          <div className="white-svg">
+            <Consultoria />
+          </div>
+          <h2>Consultoria</h2>
+        </div>
+      </section>
+
+      <section id="textresults">
+        <h3>{resultsCount} resultados encontrados</h3>
+        <h4>
+          Ordenado por:{" "}
+          <span style={{ color: "#000000", marginInline: "10px" }}>
+            Recomendados
+          </span>
+        </h4>
+      </section>
+
+      <main>
+        <div id="proposals">
+          {films.map((film, index) => (
+            <div className="proposal" key={index}>
+              <img src={film.thumbnail} alt="Thumbnail" />
+              <h3>{film.title}</h3>
+              <p>Year: {film.year}</p>
+              <p>Rating: {film.rating}</p>
+              <a href={`edit.php?id=${film.id}`}>EDIT</a>
+              <a href={`delete.php?id=${film.id}`}>DELETE</a>
+            </div>
+          ))}
+        </div>
+      </main>
+
+      <footer>
+        <div className="footer-content">
+          <div>
+            <span className="logo" style={{ color: "var(--white)" }}>
+              Freela
+              <span style={{ color: "var(--default-green)", fontSize: "3rem" }}>
+                .
+              </span>
+            </span>
+          </div>
+          <div>
+            <span style={{ color: "var(--light-gray)" }}>
+              Copyright © 2024 Freela
+            </span>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
---
+++ src/pages/icons/Edicao.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Edicao = () => (
+  <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+    <title>Mirror Icon</title>
+    <g
+      data-name="mirroring effect, effect, mirror, verticality, screen, editing tool, reflect"
+      id="mirroring_effect_effect_mirror_verticality_screen_editing_tool_reflect"
+    >
+      <path d="M29,5H17V3a1,1,0,0,0-2,0V5H14a1,1,0,0,0,0,2h1V25H14a1,1,0,0,0,0,2h1v2a1,1,0,0,0,2,0V27H29a1,1,0,0,0,1-1V6A1,1,0,0,0,29,5ZM28,25H17V7H28Z" />
+      <path d="M11,25H8a1,1,0,0,0,0,2h3a1,1,0,0,0,0-2Z" />
+      <path d="M5,25H4V24a1,1,0,0,0-2,0v2a1,1,0,0,0,1,1H5a1,1,0,0,0,0-2Z" />
+      <path d="M3,15.4a1,1,0,0,0,1-1V11.2a1,1,0,0,0-2,0v3.2A1,1,0,0,0,3,15.4Z" />
+      <path d="M3,21.8a1,1,0,0,0,1-1V17.6a1,1,0,0,0-2,0v3.2A1,1,0,0,0,3,21.8Z" />
+      <path d="M5,5H3A1,1,0,0,0,2,6V8A1,1,0,0,0,4,8V7H5A1,1,0,0,0,5,5Z" />
+      <path d="M11,5H8A1,1,0,0,0,8,7h3a1,1,0,0,0,0-2Z" />
+    </g>
+  </svg>
+);
+
+export default Edicao;
