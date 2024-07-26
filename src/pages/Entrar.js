6549293f61cad49e5dda2d868b7530cfc976bd87
uRaelM <rafael.dsm3@gmail.com>
2024-07-25 23:59:57 -0300

    deploy config, botão voltar removido por causa de bug

--- src/pages/Entrar.js
+++ src/pages/Entrar.js
@@ -63,18 +63,6 @@ export default function Cadastro() {
       </div>
       <div class="rightContainer">
         <div class="rightContent">
-          <button class="backButton" onClick={navigate("/")}>
-            <svg
-              class="icon"
-              viewBox="0 0 512 512"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g data-name="1" id="_1">
-                <path d="M353,450a15,15,0,0,1-10.61-4.39L157.5,260.71a15,15,0,0,1,0-21.21L342.39,54.6a15,15,0,1,1,21.22,21.21L189.32,250.1,363.61,424.39A15,15,0,0,1,353,450Z" />
-              </g>
-            </svg>
-            Voltar
-          </button>
           <form onSubmit={handleSubmit}>
             <h1>Entrar</h1>
             <h2>
